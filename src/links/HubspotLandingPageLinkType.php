9acd977d89e43fb8c5002cbff76866a44c156689
Chris Bovers <chris@brik.digital>
2023-12-12 21:02:01 +0100

    Translation

--- src/links/HubspotLandingPageLinkType.php
+++ src/links/HubspotLandingPageLinkType.php
@@ -21,7 +21,7 @@ class HubspotLandingPageLinkType extends Link
 
     public static function displayName(): string
     {
-        return Craft::t('hubspot', 'Hubspot Landing Page');
+        return Craft::t('craft-hubspot', 'Hubspot Landing Page');
     }
 
     public function getSettingsConfig(): array
